95f168b94f01105a609d5fd76835767fe51d171c
vineet210803 <vineet210803@gmail.com>
2025-10-11 12:03:48 +0530

    final commit

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,5 @@
-import { Box, useMediaQuery, useTheme, Typography } from "@mui/material";
-import React from "react";
+import { Box, useMediaQuery, useTheme } from "@mui/material";
 import TypingAnim from "../components/typer/TypingAnim";
-import Footer from "../components/footer/Footer";
 
 const Home = () => {
   const theme = useTheme();
